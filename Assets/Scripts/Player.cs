fe5cea4b4564886d3cd4f67bde681b95923eebd4
Enes Yapici <enesyapici.dev@gmail.com>
2025-11-04 20:57:19 +0300

    Add Jump Functionality and Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,70 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("References")]
     private Rigidbody2D rb;
+    private Animator anim;
+
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Collision Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+
     private float xInput;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
 
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
+        HandleInputs();
+        HandleCollisions();
+        HandleAnimations();
         HandleMovement();
+    }
+
+    private void HandleInputs()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            Jump();
+
+    }
+
+    private void Jump() => rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+
+
+    private void HandleCollisions()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void HandleMovement()
     {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
 
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
