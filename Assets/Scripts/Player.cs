df2fc5692245c70e5a4584679338758649f36367
Enes Yapici <enesyapici.dev@gmail.com>
2025-11-05 16:58:17 +0300

    Add Double Jump and Wall Slide Functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,30 @@
 using System;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Player : MonoBehaviour
 {
-    [Header("References")]
-    private Rigidbody2D rb;
-    private Animator anim;
+
 
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float doubleJumpForce;
+    private bool canDoubleJump;
 
     [Header("Collision Info")]
     [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float wallCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded;
-
-
+    private bool isAirborne;
+    private bool isWallDedected;
+    private bool facingRight = true;
+    private int facingDir = 1;
+    private Rigidbody2D rb;
+    private Animator anim;
     private float xInput;
+    private float yInput;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -27,27 +34,87 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        UpdateAirborneStatus();
+
         HandleInputs();
+        HandleWallSlide();
+        HandleMovement();
+        HandleFlip();
         HandleCollisions();
         HandleAnimations();
-        HandleMovement();
+    }
+
+    private void HandleWallSlide()
+    {
+        bool canWallSlide = isWallDedected && rb.linearVelocityY < 0;
+
+        float yModifier = yInput < 0 ? 1 : .05f;
+
+        if (!canWallSlide)
+            return;
+
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, rb.linearVelocityY * yModifier);
+
+    }
+
+    private void UpdateAirborneStatus()
+    {
+        if (isGrounded && isAirborne)
+        {
+            HandleLanding();
+        }
+        if (!isGrounded && !isAirborne)
+        {
+            BecomeAirborne();
+        }
+    }
+
+    private void BecomeAirborne()
+    {
+        isAirborne = true;
+    }
+
+    private void HandleLanding()
+    {
+        isAirborne = false;
+        canDoubleJump = true;
     }
 
     private void HandleInputs()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            Jump();
+        if (Input.GetKeyDown(KeyCode.Space))
+            JumpButton();
 
     }
 
+    private void JumpButton()
+    {
+        if (isGrounded)
+        {
+            Jump();
+        }
+        else if (canDoubleJump)
+        {
+            DoubleJump();
+        }
+    }
+
+
     private void Jump() => rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
 
+    private void DoubleJump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, doubleJumpForce);
+        canDoubleJump = false;
+    }
 
     private void HandleCollisions()
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDedected = Physics2D.Raycast(transform.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
     }
 
     private void HandleAnimations()
@@ -55,10 +122,25 @@ public class Player : MonoBehaviour
         anim.SetFloat("xVelocity", rb.linearVelocity.x);
         anim.SetFloat("yVelocity", rb.linearVelocity.y);
         anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isWallDedected", isWallDedected);
+    }
+    void HandleFlip()
+    {
+        if (xInput < 0 && facingRight || xInput > 0 && !facingRight)
+            Flip();
+    }
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+        facingDir = facingDir * -1;
     }
 
     private void HandleMovement()
     {
+        if (isWallDedected)
+            return;
+
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
 
     }
@@ -66,5 +148,6 @@ public class Player : MonoBehaviour
     void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + (wallCheckDistance * facingDir), transform.position.y));
     }
 }
